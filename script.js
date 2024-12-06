3bd24489394be05b584a9791484784d7639fe3a2
Sachinkisna <sachinsoftware0001@gmail.com>
2024-12-06 16:46:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let marks;
+marks=prompt("Enter Your Mark: Below");
+if(marks<=100 && marks>=80){
+        console.log("Your Grade IS A");
+}else if(marks<=89 && marks>=70){
+    console.log("Your Grade IS B");
+}else if(marks<=69 && marks>=60){
+    console.log("Your Grade IS C");
+}else if(marks<=59 && marks>=50){
+    console.log("Your Grade IS C");
+}else{
+    console.log("You Are Failed");
+};
